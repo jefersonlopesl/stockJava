afc7c0296bb91a8e4c9933ff50fa6c6859051ff7
Jeferson Lopes <jefersonlopesluis@protonmail.com>
2023-01-18 11:25:38 -0300

    adicionei o metodo toString

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,9 @@ public class Main {
         System.out.print("Quantity in stock: ");
         product.quantity = sc.nextInt();
 
-        System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+        System.out.println(product);
+
+
 
         sc.close();
     }
--- src/Product.java
+++ src/Product.java
@@ -13,4 +13,13 @@ public class Product {
     public void removeProduct(int quantity) {
         this.quantity -= quantity;
     }
+    public String toString() {
+        return name
+                + ", $ "
+                + String.format("%.2f", price)
+                + ", "
+                + quantity
+                + " units, Total: $ "
+                + String.format("%.2f", totalValueInStock());
+    }
 }
